762ba5eab7b696727894f2eccbaabeef5e33adcf
Aryaman Dhingra <aryamandhingra@gmail.com>
2021-05-16 16:55:55 +0400

    set up routes + changes to header

--- src/App.js
+++ src/App.js
@@ -8,6 +8,12 @@ function App() {
     <Router>
       <Switch>
         <Route exact path="/" component={Header} />
+        <Route exact path="/about" component={Header} />
+        <Route exact path="/skills" component={Header} />
+        <Route exact path="/projects" component={Header} />
+        <Route exact path="/progress" component={Header} />
+        <Route exact path="/resume" component={Header} />
+        <Route exact path="/socials" component={Header} />
       </Switch>
     </Router>
 
--- src/pages/Header.js
+++ src/pages/Header.js
@@ -7,13 +7,15 @@ export default function Header() {
         <div class="max-w-screen-xl mx-auto px-4 flex items-center lg:h-screen">
             <div class="-mx-4 flex flex-wrap">
                 <div class="w-full flex flex-col p-4 sm:w-1/2 lg:w-1/4">
-                    <div class="flex-1 px-5 py-6 bg-white rounded-lg shadow-lg sm:h-1/4 md:h-full bg-grey-dark" >
-                        <p class="text-4xl">About me</p>
-                    </div>
+                    <Link to="/about" class="w-full flex flex-col h-full">
+                        <div class="flex-1 px-5 py-6 rounded-lg shadow-lg sm:h-1/4 md:h-full bg-grey-dark hover:bg-blue-light transition duration-300" >
+                            <p class="text-4xl">About me</p>
+                        </div>
+                    </Link>
                 </div>
                 <div class="w-full flex flex-col p-4 sm:w-1/2 lg:w-1/2">
-                    <Link to="/">
-                        <div class="flex-1 px-5 py-6 bg-white rounded-lg shadow-lg bg-grey-dark" >
+                    <Link to="/" class="w-full flex flex-col h-full">
+                        <div class="flex-1 px-5 py-6 rounded-lg shadow-lg sm:h-1/4 md:h-full bg-grey-dark hover:bg-blue-light transition duration-300">
                             <p class="text-4xl">Hi, I'm Aryaman. </p>
                             <br></br>
                             <p class="text-xl">I'm a Computer Science student at the University of Waterloo.</p>
@@ -23,32 +25,42 @@ export default function Header() {
                         </div>
                     </Link>
                 </div>
-                <div class="w-full flex flex-col p-4 sm:w-1/2 lg:w-1/4">
-                    <div class="flex-1 px-5 py-6 bg-white rounded-lg shadow-lg bg-grey-dark">
-                        <p class="text-4xl">Socials</p>
-                    </div>
+                <div class="w-full flex flex-col p-4 sm:w-1/2 lg:w-1/4 ">
+                    <Link to="/socials" class="w-full flex flex-col h-full">
+                        <div class="flex-1 px-5 py-6 rounded-lg shadow-lg sm:h-1/4 md:h-full bg-grey-dark hover:bg-blue-light transition duration-300">
+                            <p class="text-4xl">Socials</p>
+                        </div>
+                    </Link>
                 </div>
                 <div class="w-full flex flex-col p-4 sm:w-1/2 lg:w-1/4">
-                    <div class="flex-1 px-5 py-6 bg-white rounded-lg shadow-lg bg-grey-dark">
-                        <p class="text-4xl">Skills and Proficiencies</p>
-                    </div>
+                    <Link to="/skills" class="w-full flex flex-col h-full">
+                        <div class="flex-1 px-5 py-6 rounded-lg shadow-lg sm:h-1/4 md:h-full bg-grey-dark hover:bg-blue-light transition duration-300">
+                            <p class="text-4xl">Skills</p>
+                        </div>
+                    </Link>
                 </div>
                 <div class="w-full flex flex-col p-4 sm:w-1/2 lg:w-1/4">
-                    <div class="flex-1 px-5 py-6 bg-white rounded-lg shadow-lg sm:h-1/4 md:h-full bg-grey-dark">
-                        <p class="text-4xl">Completed Projects</p>
-                        <p class="text-xl">View a list + description of the projects that I have stopped working on</p>
-                        <p class="text-xl"> (for now!)</p>
-                    </div>
+                    <Link to="/projects" class="w-full flex flex-col h-full">
+                        <div class="flex-1 px-5 py-6  rounded-lg shadow-lg sm:h-1/4 md:h-full bg-grey-dark hover:bg-blue-light transition duration-300">
+                            <p class="text-4xl">Completed Projects</p>
+                            <p class="text-xl">View a list + description of the projects that I have stopped working on</p>
+                            <p class="text-xl"> (for now!)</p>
+                        </div>
+                    </Link>
                 </div>
                 <div class="w-full flex flex-col p-4 sm:w-1/2 lg:w-1/4">
-                    <div class="flex-1 px-5 py-6 bg-white rounded-lg shadow-lg bg-grey-dark">
-                        <p class="text-4xl">Currently working on...</p>
-                    </div>
+                    <Link to="/progress" class="w-full flex flex-col h-full">
+                        <div class="flex-1 px-5 py-6 rounded-lg shadow-lg sm:h-1/4 md:h-full bg-grey-dark hover:bg-blue-light transition duration-300">
+                            <p class="text-4xl">Currently working on...</p>
+                        </div>
+                    </Link>
                 </div>
                 <div class="w-full flex flex-col p-4 sm:w-1/2 lg:w-1/4">
-                    <div class="flex-1 px-5 py-6 bg-white rounded-lg shadow-lg bg-blue-light">
-                        <p class="text-4xl">Resume</p>
-                    </div>
+                    <Link to="/resume" class="w-full flex flex-col h-full">
+                        <div class="flex-1 px-5 py-6 rounded-lg shadow-lg sm:h-1/4 md:h-full bg-blue-light hover:bg-blue-dark transition duration-300">
+                            <p class="text-4xl">Resume</p>
+                        </div>
+                    </Link>
                 </div>
             </div>
         </div>
