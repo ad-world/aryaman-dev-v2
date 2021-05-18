e6e563a3cec006deac2a32258087c853ffcb4b52
Aryaman Dhingra <aryamandhingra@gmail.com>
2021-05-18 16:52:27 +0400

    divided header

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -1,70 +1,70 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import Github from '../img/github.svg';
+import React, { useState } from 'react'
+import About from './About'
+import Socials from './Socials'
+import Skills from './Skills'
 
 export default function Header() {
+    const [current, setCurrent] = useState(<About />);
     return (
-        <div class="max-w-screen-xl mx-auto px-4 flex items-center lg:h-screen ">
-            <div class="-mx-4 flex flex-wrap">
-
-                <div class="w-full flex flex-col p-4 sm:w-1/2 lg:w-1/2">
-                    <div class="flex-1 px-5 py-6 rounded-lg shadow-lg sm:h-1/4 md:h-full bg-grey-dark">
-                        <p class="text-4xl">Hi, I'm Aryaman. </p>
-                        <br></br>
-                        <p class="text-xl">I'm a Computer Science student at the University of Waterloo.</p>
-                        <p class="text-xl">I will be going into my second year in Fall 2021.</p>
-                        <br></br>
-                        <p class="text-xl">Navigate through this page to learn a little bit more about me!</p>
-                        <br></br>
+        <div class="md:grid md:grid-cols-2 lg:divide-x md:divide-y-8 divide-none visible">
+            <div class="w-3/4 mx-auto px-4 flex items-center lg:h-screen md:h-auto">
+                <div class="-mx-4 flex flex-wrap">
+                    <div class="w-full flex flex-col p-4 sm:w-full lg:w-full">
+                        <div class="flex-1 px-5 py-6 rounded-lg shadow-lg sm:h-1/4 md:h-1/4 bg-grey-dark object-contain hover:bg-blue-light transition duration-300"
+                            onClick={() => setCurrent(<About />)}>
+                            <p class="text-4xl">Hi, I'm Aryaman. </p>
+                            <br></br>
+                            <p class="text-xl">I'm a Computer Science student at the University of Waterloo.</p>
+                            <p class="text-xl">I will be going into my second year in Fall 2021.</p>
+                            <br></br>
+                            <p class="text-xl">Navigate through this page to learn a little bit more about me!</p>
+                        </div>
                     </div>
-                </div>
-                <div class="w-full flex flex-col p-4 sm:w-1/2 lg:w-1/4 ">
-                    <Link to="/socials" class="w-full flex flex-col h-full">
-                        <div class="flex-1 px-5 py-6 rounded-lg shadow-lg sm:h-1/4 md:h-full bg-grey-dark hover:bg-blue-light transition duration-300">
+                    <div class="w-full flex flex-col p-4 md:w-full lg:w-1/2 ">
+                        <div class="flex-1 px-5 py-6 rounded-lg shadow-lg sm:h-1/4 md:h-full bg-grey-dark hover:bg-blue-light transition duration-300"
+                            onClick={() => setCurrent(<Socials />)}>
                             <p class="text-4xl">Say Hello</p>
                         </div>
-                    </Link>
-                </div>
-                <div class="w-full flex flex-col p-4 sm:w-1/2 lg:w-1/4">
-                    <a href="https://github.com/ad-world" target="_blank" rel="noreferrer" class="w-full flex flex-col h-full">
-                        <div class="flex-1 px-5 py-6 rounded-lg shadow-lg sm:h-1/4 md:h-full bg-blue-light hover:bg-blue-dark transition duration-300 align-middle my">
-                            <div class="h-3/4 w-3/4 flex justify-center mx-auto align-middle inline-block">
-                                <img src={Github} class="mx-auto" />
-                            </div>
+                    </div>
+                    <div class="w-full flex flex-col p-4 md:w-full lg:w-1/2">
+                        <div class="flex-1 px-5 py-6 rounded-lg shadow-lg sm:h-1/4 md:h-full bg-grey-dark hover:bg-blue-light transition duration-300 align-middle my">
+                            <p class="text-4xl">
+                                Currently working on...
+                            </p>
                         </div>
-                    </a>
-                </div>
-                <div class="w-full flex flex-col p-4 sm:w-1/2 lg:w-1/4">
-                    <Link to="/skills" class="w-full flex flex-col h-full">
-                        <div class="flex-1 px-5 py-6 rounded-lg shadow-lg sm:h-1/4 md:h-full bg-grey-dark hover:bg-blue-light transition duration-300">
+                    </div>
+                    <div class="w-full flex flex-col p-4 md:w-full lg:w-1/2">
+                        <div class="flex-1 px-5 py-6 rounded-lg shadow-lg sm:h-1/4 md:h-full bg-grey-dark hover:bg-blue-light transition duration-300"
+                        onClick={() => setCurrent(<Skills/>)}>
                             <p class="text-4xl">Skills</p>
                         </div>
-                    </Link>
-                </div>
-                <div class="w-full flex flex-col p-4 sm:w-1/2 lg:w-1/4">
-                    <Link to="/projects" class="w-full flex flex-col h-full">
+                    </div>
+                    <div class="w-full flex flex-col p-4 md:w-full lg:w-1/2">
                         <div class="flex-1 px-5 py-6  rounded-lg shadow-lg sm:h-1/4 md:h-full bg-grey-dark hover:bg-blue-light transition duration-300">
                             <p class="text-4xl">Completed Projects</p>
                             <br></br>
                             <p class="text-xl">View a list + description of the projects that I have completed (for now!)</p>
                         </div>
-                    </Link>
-                </div>
-                <div class="w-full flex flex-col p-4 sm:w-1/2 lg:w-1/4">
-                    <Link to="/progress" class="w-full flex flex-col h-full">
-                        <div class="flex-1 px-5 py-6 rounded-lg shadow-lg sm:h-1/4 md:h-full bg-grey-dark hover:bg-blue-light transition duration-300">
-                            <p class="text-4xl">Currently working on...</p>
-                        </div>
-                    </Link>
-                </div>
-                <div class="w-full flex flex-col p-4 sm:w-1/2 lg:w-1/4">
-                    <Link to="/resume" class="w-full flex flex-col h-full">
+
+                    </div>
+                    <div class="w-full flex flex-col p-4 md:w-full lg:w-1/2">
+                        <a href="https://github.com/ad-world" target="_blank" rel="noreferrer" class="h-full">
+                            <div class="flex-1 px-5 py-6 rounded-lg shadow-lg sm:h-1/4 md:h-full bg-blue-light hover:bg-blue-dark transition duration-300">
+
+                                <p class="text-4xl">Github</p>
+
+                            </div>
+                        </a>
+                    </div>
+                    <div class="w-full flex flex-col p-4 md:w-full lg:w-1/2">
                         <div class="flex-1 px-5 py-6 rounded-lg shadow-lg sm:h-1/4 md:h-full bg-blue-light hover:bg-blue-dark transition duration-300">
                             <p class="text-4xl">Resume</p>
                         </div>
-                    </Link>
+                    </div>
                 </div>
             </div>
+            {current}
         </div>
+
     )
 }
