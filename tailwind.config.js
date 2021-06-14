d83c7298b7b932cc10afd45c1ddee004ba0e1bf7
Aryaman Dhingra <aryamandhingra@gmail.com>
2021-06-14 21:00:02 +0400

    Add light mode colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,29 @@
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: 'class', // or 'media' or 'class'
-  theme: {
-    container: {
-      center: true,
-    },
-    colors: {
-      blue: {
-        dark: '#5f85db',
-        light: '#90b8f8',
-      },
-      grey: {
-        dark: '#26282b',
-        light: '#353941'
-      },
-      white: '#FFFFFF'
-    },
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+	purge    : [ './src/**/*.{js,jsx,ts,tsx}', './public/index.html' ],
+	darkMode : 'class', // or 'media' or 'class'
+	theme    : {
+		container : {
+			center : true
+		},
+		colors    : {
+			blue  : {
+				dark  : '#5f85db',
+				light : '#90b8f8'
+			},
+			grey  : {
+				dark  : '#26282b',
+				light : '#353941',
+				main  : '#AAAAAA'
+			},
+			white : '#F4F9F9',
+			sky   : {
+				dark  : '#A4EBF3',
+				light : '#CCF2F4'
+			}
+		}
+	},
+	variants : {
+		extend : {}
+	},
+	plugins  : []
+};
